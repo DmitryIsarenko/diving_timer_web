4059fc65654deeea188da9693967dbe365d38bac
Isarenko Dmitry <isarenko.dmitry.it@gmail.com>
2025-11-03 21:57:25 +0300

    Настроен UI

--- timer/urls.py
+++ timer/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, exercise_prep, exercise, exercise_statistics
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('exercise-prep/', exercise_prep, name='exercise_prep'),
+    path('exercise/', exercise, name='exercise'),
+    path('exercise-statistics/', exercise_statistics, name='exercise_statistics'),
 ]
 
 
--- timer/views.py
+++ timer/views.py
@@ -6,3 +6,15 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'timer/index.html')
 
 
+def exercise_prep(request: HttpRequest) -> HttpResponse:
+    return render(request, 'timer/exercise_prep.html')
+
+
+def exercise(request: HttpRequest) -> HttpResponse:
+    return render(request, 'timer/exercise.html')
+
+
+def exercise_statistics(request: HttpRequest) -> HttpResponse:
+    return render(request, 'timer/exercise_statistics.html')
+
+
